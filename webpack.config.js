8b05700d956e1ab9a71d2c56d1645b15f065dace
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-03-16 21:41:49 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production'
+}
+
+module.exports = {
+  mode: mode,
+  output: {
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          (mode === 'development') ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                  ],
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
+}
